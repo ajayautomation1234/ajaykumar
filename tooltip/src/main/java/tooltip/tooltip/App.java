1737daf2e4874f03e90d0834dc441745b49eb310
ajay <ajayam1234@gmail.com>
2021-08-19 16:45:08 +0530

    hi

---
+++ tooltip/src/main/java/tooltip/tooltip/App.java
@@ -0,0 +1,13 @@
+package tooltip.tooltip;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
